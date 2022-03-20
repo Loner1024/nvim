e07df76e10e52aa6fe97b53689ae072f4b0c813d
Loner1024 <xth12138@gmail.com>
2022-03-21 00:36:29 +0800

    refactor

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,7 +1,8 @@
-local colorscheme = "dafault"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not fount!")
-  return
-end
+vim.cmd [[
+try
+  colorscheme darkplus
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
