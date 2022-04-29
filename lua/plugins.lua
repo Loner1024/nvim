8c6a531a8ffd7ef59488359fe23d61d5eb9fd443
解添昊 <xietianhao@pudutech.com>
2022-04-29 16:24:53 +0800

    change config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,87 +1,177 @@
+-- 自动安装 Packer.nvim
+-- 插件安装目录
+-- ~/.local/share/nvim/site/pack/packer/
 local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local paccker_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
+  vim.notify("正在安装Pakcer.nvim，请稍后...")
+  paccker_bootstrap = fn.system({
     "git",
     "clone",
     "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
+    "1", -- "https://github.com/wbthomason/packer.nvim",
+    "https://gitcode.net/mirrors/wbthomason/packer.nvim",
     install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
-end
+  })
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+  -- https://github.com/wbthomason/packer.nvim/issues/750
+  local rtp_addition = vim.fn.stdpath("data") .. "/site/pack/*/start/*"
+  if not string.find(vim.o.runtimepath, rtp_addition) then
+    vim.o.runtimepath = rtp_addition .. "," .. vim.o.runtimepath
+  end
+  vim.notify("Pakcer.nvim 安装完毕")
+end
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
+  vim.notify("没有安装 packer.nvim")
   return
 end
 
--- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
+packer.startup({
+  function(use)
+    -- Packer 可以升级自己
+    use("wbthomason/packer.nvim")
+    -------------------------- plugins -------------------------------------------
+    -- nvim-tree
+    use({
+      "kyazdani42/nvim-tree.lua",
+      requires = "kyazdani42/nvim-web-devicons",
+    })
+    -- bufferline
+    use({
+      "akinsho/bufferline.nvim",
+      requires = { "kyazdani42/nvim-web-devicons", "moll/vim-bbye" },
+    })
+    -- lualine
+    use({
+      "nvim-lualine/lualine.nvim",
+      requires = { "kyazdani42/nvim-web-devicons" },
+    })
+    use("arkav/lualine-lsp-progress")
+    -- telescope
+    use({
+      "nvim-telescope/telescope.nvim",
+      requires = { "nvim-lua/plenary.nvim" },
+    })
+    -- telescope extensions
+    use("LinArcX/telescope-env.nvim")
+    use("nvim-telescope/telescope-ui-select.nvim")
+    -- dashboard-nvim
+    use("glepnir/dashboard-nvim")
+    -- project
+    use("ahmedkhalf/project.nvim")
+    -- treesitter
+    use({
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+    })
+    use("p00f/nvim-ts-rainbow")
+    -- indent-blankline
+    use("lukas-reineke/indent-blankline.nvim")
+    --------------------- LSP --------------------
+    -- Lspconfig
+    use({ "neovim/nvim-lspconfig", "williamboman/nvim-lsp-installer" })
+    -- 补全引擎
+    use("hrsh7th/nvim-cmp")
+    -- Snippet 引擎
+    use("hrsh7th/vim-vsnip")
+    -- 补全源
+    use("hrsh7th/cmp-vsnip")
+    use("hrsh7th/cmp-nvim-lsp") -- { name = nvim_lsp }
+    use("hrsh7th/cmp-buffer") -- { name = 'buffer' },
+    use("hrsh7th/cmp-path") -- { name = 'path' }
+    use("hrsh7th/cmp-cmdline") -- { name = 'cmdline' }
+    use("hrsh7th/cmp-nvim-lsp-signature-help") -- { name = 'nvim_lsp_signature_help' }
+    -- 常见编程语言代码段
+    use("rafamadriz/friendly-snippets")
+    -- UI 增强
+    use("onsails/lspkind-nvim")
+    use("tami5/lspsaga.nvim")
+    -- 代码格式化
+    use("mhartington/formatter.nvim")
+    use({ "jose-elias-alvarez/null-ls.nvim", requires = "nvim-lua/plenary.nvim" })
+    -- TypeScript 增强
+    use({ "jose-elias-alvarez/nvim-lsp-ts-utils", requires = "nvim-lua/plenary.nvim" })
+    -- Lua 增强
+    use("folke/lua-dev.nvim")
+    -- JSON 增强
+    use("b0o/schemastore.nvim")
+    -- Rust 增强
+    use("simrat39/rust-tools.nvim")
+    use("wakatime/vim-wakatime")
+
+    --------------------- colorschemes --------------------
+    -- tokyonight
+    use("folke/tokyonight.nvim")
+    -- OceanicNext
+    use("mhartington/oceanic-next")
+    -- gruvbox
+    use({
+      "ellisonleao/gruvbox.nvim",
+      requires = { "rktjmp/lush.nvim" },
+    })
+    -- zephyr
+    -- use("glepnir/zephyr-nvim")
+    -- nord
+    use("shaunsingh/nord.nvim")
+    -- onedark
+    use("ful1e5/onedark.nvim")
+    -- nightfox
+    use("EdenEast/nightfox.nvim")
+
+    -------------------------------------------------------
+    use({ "akinsho/toggleterm.nvim" })
+    -- surround
+    use("ur4ltz/surround.nvim")
+    -- Comment
+    use("numToStr/Comment.nvim")
+    -- nvim-autopairs
+    use("windwp/nvim-autopairs")
+    -- git
+    use({ "lewis6991/gitsigns.nvim" })
+    -- vimspector
+    use("puremourning/vimspector")
+    ----------------------------------------------
+    use("mfussenegger/nvim-dap")
+    use("theHamsta/nvim-dap-virtual-text")
+    use("rcarriga/nvim-dap-ui")
+    -- use("Pocco81/DAPInstall.nvim")
+    -- use("jbyuki/one-small-step-for-vimkind")
+
+    if paccker_bootstrap then
+      packer.sync()
+    end
+  end,
+  config = {
+    -- 最大并发数
+    max_jobs = 16,
+    -- 自定义源
+    git = {
+      -- default_url_format = "https://hub.fastgit.xyz/%s",
+      -- default_url_format = "https://mirror.ghproxy.com/https://github.com/%s",
+      -- default_url_format = "https://gitcode.net/mirrors/%s",
+      -- default_url_format = "https://gitclone.com/github.com/%s",
+    },
+    -- display = {
+    -- 使用浮动窗口显示
+    --   open_fn = function()
+    --     return require("packer.util").float({ border = "single" })
+    --   end,
+    -- },
   },
-}
+})
 
--- Install your plugins here
-return packer.startup(function(use)
-  -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  use 'wakatime/vim-wakatime'
-
-  -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
-
-  -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
-
-  -- nvim-tree
-  use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
-  use ({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-
-  -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
-
-  -- LSP
-  use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-
-  -- Golang
-  use('crispgm/nvim-go')
-  use('rcarriga/nvim-notify')
-
-  use "windwp/nvim-autopairs" 
-
-
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+-- 每次保存 plugins.lua 自动安装插件
+-- move to autocmds.lua
+-- pcall(
+--   vim.cmd,
+--   [[
+-- augroup packer_user_config
+-- autocmd!
+-- autocmd BufWritePost plugins.lua source <afile> | PackerSync
+-- augroup end
+-- ]]
+-- )
